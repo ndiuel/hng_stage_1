481d23aab0b8b2f989e6c4ba96bdcac4456e0384
unknown <ndiuel@gmail.com>
2023-09-11 20:48:13 +0100

    bodyparser

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
 
 const days = {
     0: 'Sunday',
